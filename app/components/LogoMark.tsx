4da08d80560b7fbd4e5ce13623fecd45180a1c33
Doryc Local <local@doryc.invalid>
2026-08-31 14:16:06 -0500

    feat: adopt striped Doryc brand mark

--- app/components/LogoMark.tsx
+++ app/components/LogoMark.tsx
@@ -2,8 +2,15 @@ export default function LogoMark() {
   return (
     <span className="brand-mark" aria-hidden="true">
       <svg viewBox="0 0 40 40" fill="none">
-        <path className="logo-coin" fillRule="evenodd" d="M8.5 7.5h10.3C26 7.5 30.5 11.9 30.5 20s-4.5 12.5-11.7 12.5H8.5v-25Zm7 6.5v12h3.1c3.4 0 5.3-2.1 5.3-6s-1.9-6-5.3-6h-3.1Z" />
-        <circle className="logo-cut" cx="11.7" cy="20" r="2" />
+        <g className="logo-lines">
+          <path d="M11 8.5h11" />
+          <path d="M8.5 12.5h17" />
+          <path d="M7 16.5h21" />
+          <path d="M7 24.5h21" />
+          <path d="M8.5 28.5h17" />
+          <path d="M11 32.5h11" />
+        </g>
+        <path className="logo-accent" d="M6.5 20.5h22" />
       </svg>
     </span>
   );
