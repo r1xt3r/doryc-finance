017ced43aebfaefd4373a397ffa2be1707a27fb9
Doryc Local <local@doryc.invalid>
2026-08-31 14:28:00 -0500

    feat: restore the Midnight Doryc logo

--- app/components/LogoMark.tsx
+++ app/components/LogoMark.tsx
@@ -2,16 +2,9 @@ export default function LogoMark() {
   return (
     <span className="brand-mark" aria-hidden="true">
       <svg viewBox="0 0 40 40" fill="none">
-        <g className="logo-lines">
-          <path d="M8 7.5h13" />
-          <path d="M8 11.5h18" />
-          <path d="M8 15.5h21" />
-          <path d="M8 19.5h22" />
-          <path d="M8 27.5h21" />
-          <path d="M8 31.5h18" />
-          <path d="M8 35.5h13" />
-        </g>
-        <path className="logo-accent" d="M8 23.5h22.5" />
+        <path className="logo-d" fillRule="evenodd" d="M12 7h8.5C27.3 7 32 11.8 32 20s-4.7 13-11.5 13H12V7Zm6 6.2v13.6h2.2c3.7 0 5.8-2.3 5.8-6.8s-2.1-6.8-5.8-6.8H18Z" />
+        <path className="logo-growth-shadow" d="m7.5 27 3.7-3.7 2.7 2.5 5.1-5.8" />
+        <path className="logo-growth" d="m7.5 27 3.7-3.7 2.7 2.5 5.1-5.8m-3.4 0H19v3.4" />
       </svg>
     </span>
   );
